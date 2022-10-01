c9281ed18d3cdecc87795e6913467a4816f1e27f
Ayush Panwar <ayushpanwar691@gmail.com>
2022-10-01 08:15:41 +0530

    Added a utility to clear text using react hooks.

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,17 +1,25 @@
 import React, { useState } from 'react'
 
 export default function TextForm(props) {
+    const [text, setText] = useState('');
 
+    const changeHandler = (e) => {
+        setText(e.target.value);
+    }
+
+    const clearTextHandler = () => {
+        setText('');
+    }
     return (
         <>
             <div className='container'>
                 <h4>{props.heading}</h4>
                 <div className="mb-3">
-                    <textarea className="form-control" v  id="exampleFormControlTextarea1" rows="16"></textarea>
+                    <textarea className="form-control" v  id="exampleFormControlTextarea1" rows="16" value={text}  onChange={changeHandler}></textarea>
                 </div>
                 <button className="btn btn-primary mx-1 mb-1" >Convert to Upper Case</button>
                 <button className="btn btn-primary mx-1 mb-1"  >Convert to Lower Case</button>
-                <button className="btn btn-primary mx-1 mb-1"  >Clear Text</button>
+                <button className="btn btn-primary mx-1 mb-1"  onClick={clearTextHandler}>Clear Text</button>
             </div>
 
 
