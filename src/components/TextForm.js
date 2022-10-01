0c0d02b1cb9db5fac4833bd51184f24bb253bd34
Ayush Panwar <ayushpanwar691@gmail.com>
2022-10-01 09:25:54 +0530

    Added placeholder

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -15,7 +15,7 @@ export default function TextForm(props) {
             <div className='container'>
                 <h4>{props.heading}</h4>
                 <div className="mb-3">
-                    <textarea className="form-control" v  id="exampleFormControlTextarea1" rows="16" value={text}  onChange={changeHandler}></textarea>
+                    <textarea className="form-control" v  id="exampleFormControlTextarea1" rows="16" value={text}  onChange={changeHandler} placeholder="Enter the text"></textarea>
                 </div>
                 <button className="btn btn-primary mx-1 mb-1" >Convert to Upper Case</button>
                 <button className="btn btn-primary mx-1 mb-1"  >Convert to Lower Case</button>
