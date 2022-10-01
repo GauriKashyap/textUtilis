bb40833028b3005f5a19477626e1b6a4c9a016a2
Varun Singh <varun7singh10@gmail.com>
2022-10-01 07:40:45 +0530

    feat: added feature

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,15 +1,28 @@
 import React, { useState } from 'react'
 
 export default function TextForm(props) {
+    const [text, setText] = useState('');
+
+    const changeHandler = (e) => {
+        setText(e.target.value);
+    }
+
+    const upperCaseHandler = () => {
+        let textArr = text.split(' ');
+        let newText = textArr.map((word) => {
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        })
+        setText(newText.join(' '));
+    }   
 
     return (
         <>
             <div className='container'>
                 <h4>{props.heading}</h4>
                 <div className="mb-3">
-                    <textarea className="form-control" v  id="exampleFormControlTextarea1" rows="16"></textarea>
+                    <textarea className="form-control"  v id="exampleFormControlTextarea1" rows="16" value={text} onChange={changeHandler}></textarea>
                 </div>
-                <button className="btn btn-primary mx-1 mb-1" >Convert to Upper Case</button>
+                <button className="btn btn-primary mx-1 mb-1" onClick={upperCaseHandler}>Convert to Upper Case</button>
                 <button className="btn btn-primary mx-1 mb-1"  >Convert to Lower Case</button>
                 <button className="btn btn-primary mx-1 mb-1"  >Clear Text</button>
             </div>
