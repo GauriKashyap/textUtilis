f14c4ddd861473b7fa225e970ba2d5b047bd57c9
varshneyprateek20@gmail.com <varshneyprateek20@gmail.com>
2022-10-02 11:14:12 +0530

    Added Text to Audio Feature

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,11 +1,13 @@
 import React, { useState, useRef } from 'react';
 import useHighlightText from '../hooks/useHighlightText';
+import { useSpeechSynthesis } from 'react-speech-kit';
 
 export default function TextForm(props) {
     const [text, setText] = useState('This is a default text.');
     const [isFormatUponHighlighted, setIsFormatUponHighlighted] = useState(false);
     const textAreaRef = useRef(null);
     const { highlightedText, onClearHighlightedText } = useHighlightText(textAreaRef);
+    const { speak } = useSpeechSynthesis();
     // useState is a hook in React which is used to initialize a state variable. text is the default value 
     // text = "n"; this would have worked if we would have used "var" or "let" instead of "const"
     //text ="random";  wrong way to update the state of a variable. We need a function like setText() which will be upgrading the value of the variable "text".
@@ -35,7 +37,7 @@ export default function TextForm(props) {
         console.log("Upper Case was clicked " + text);
 
         let result = text.toUpperCase();
-        const { nonSelectedFrontText,  selectedText, nonSelectedBackText} = separateText(text, highlightedText);
+        const { nonSelectedFrontText, selectedText, nonSelectedBackText } = separateText(text, highlightedText);
 
         if (isFormatUponHighlighted) {
             result = nonSelectedFrontText + selectedText.toUpperCase() + nonSelectedBackText;
@@ -46,8 +48,8 @@ export default function TextForm(props) {
         console.log("Upper Case was clicked " + text);
 
         let result = text.toLowerCase();
-        const { nonSelectedFrontText,  selectedText, nonSelectedBackText} = separateText(text, highlightedText);
-        
+        const { nonSelectedFrontText, selectedText, nonSelectedBackText } = separateText(text, highlightedText);
+
         if (isFormatUponHighlighted) {
             result = nonSelectedFrontText + selectedText.toLowerCase() + nonSelectedBackText;
         }
@@ -57,7 +59,7 @@ export default function TextForm(props) {
 
         let result = capitalize(text);
 
-        const { nonSelectedFrontText,  selectedText, nonSelectedBackText} = separateText(text, highlightedText);
+        const { nonSelectedFrontText, selectedText, nonSelectedBackText } = separateText(text, highlightedText);
 
         if (isFormatUponHighlighted) {
             result = nonSelectedFrontText + capitalize(selectedText) + nonSelectedBackText;
@@ -74,6 +76,9 @@ export default function TextForm(props) {
         console.log("Some change has been made in the text box ");
         setText(event.target.value);
         // This is basiclly used to update the value of text area in the textBox as the variable const text is a state and by default the value of this state variable text is default which is "This is a default text", and if we want to enable this value, which is only possible using onChange function in the textbox, so we need to tell react if the user types something in the textArea, which bascially means triggerr ing onChange function in textArea (onChange={handleOnChange}), so by mentioning "setText(event.target.value)", we will tell react to record this event and hence record the changes from the user and hence records the event and change the value of original state variable text using the above setText function.
+    }
+    const handleTextToAudio = () => {
+        speak({ text });
     }
 
     console.log(isFormatUponHighlighted);
@@ -96,13 +101,14 @@ export default function TextForm(props) {
                 <button className="btn btn-primary mx-1 mb-1" onClick={handleLoClick} >Convert to Lower Case</button>
                 <button className="btn btn-primary mx-1 mb-1" onClick={handleCaClick} >Capitalize</button>
                 <button className="btn btn-primary mx-1 mb-1" onClick={handleClearClick} >Clear Text</button>
+                <button className="btn btn-primary mx-1 mb-1" onClick={handleTextToAudio}>Listen Now</button>
                 <div>
                     <input type="checkbox" onChange={(e) => setIsFormatUponHighlighted(e.target.checked)} checked={isFormatUponHighlighted} />
                     <label className="mx-1 mt-2">Format highlighted only</label>
                 </div>
             </div>
             {/* Number of words and characters feature.*/}
-           
+
             <div className="container my-3">
                 <h4>Your Text Summary:</h4>
                 <p><em>{text.split(" ").length} words and {text.length} characters.</em></p>
